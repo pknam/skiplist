3fbfedc4b7fc3c53de2934938f5700e26ea96e94
Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>
2019-08-12 16:53:43 +0900

    head: change types of key and value for consistency
    Int32 is weird...
    
    Signed-off-by: Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>

--- SkipList/ConcurrentSkipListMapHeadNode.cs
+++ SkipList/ConcurrentSkipListMapHeadNode.cs
@@ -4,8 +4,8 @@ namespace SkipList
 {
     internal class ConcurrentSkipListMapHeadNode<TKey, TValue> : ConcurrentSkipListMapNode<TKey, TValue>
     {
-        public new Int32 Key => throw new NotImplementedException();
-        public new Int32 Value => throw new NotImplementedException();
+        public new TKey Key => throw new NotImplementedException();
+        public new TValue Value => throw new NotImplementedException();
 
         public ConcurrentSkipListMapHeadNode(Int32 forwardLength)
             :base(forwardLength)
